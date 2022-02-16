af6f3807bad0d2fe2084c551b754437553b8581e
PhDev <henriquedsg15@gmail.com>
2022-02-16 19:54:22 -0300

    commitSalarioDois

---
+++ src/GitTeste.java
@@ -0,0 +1,16 @@
+
+public class GitTeste {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hllo World");
+		
+		int x = 528;
+		int y = 382;
+		String T = "Teste";
+		
+		System.out.println((x*y) + "\n" + (x/y) + "\n" + T);
+
+	}
+
+}
